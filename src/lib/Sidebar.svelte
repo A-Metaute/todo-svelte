e07818c5b28e0174c7dfce309e36952cf68f8037
Arnau Metauete Carrillo <ametauet@emeal.nttdata.com>
2022-03-30 12:45:09 +0200

    Svelte to-do project

---
+++ src/lib/Sidebar.svelte
@@ -0,0 +1,58 @@
+<script>
+    import { links, Router } from "svelte-routing";
+    </script>
+
+<div>
+    <img alt="Svelte logo" src="src/assets/svelte.png" />
+    <p>Svelte ToDo</p>
+</div>
+
+<nav use:links>
+    <Router>
+        <a href="/">Show tasks</a>
+        <a href="/add">Add task</a>
+    </Router>
+    
+</nav>
+
+<style>
+div{
+    grid-column: 1 / 2;
+    display: flex;
+    flex-direction: row;
+    align-items: center;
+    gap: 2rem;
+}
+
+img{
+    width: 48px;
+    height: 48px;
+    margin-left: 20px;
+}
+
+p{
+    display:inline-block;
+    font-weight: bold;
+    font-size: 2rem;
+}
+
+nav{
+    grid-column: 2 / 3;
+    display: flex;
+    flex-direction: row;
+    align-items: center;
+    justify-content: center;
+    gap: 5rem;
+}
+
+a{
+    text-decoration: none;
+    font-size: 2rem;
+    font-weight: bold;
+    color: black;
+}
+
+a:hover {
+    color: #ff3e00;
+}
+</style>
